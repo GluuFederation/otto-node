250eac13bb01849ed7df54b990dd090070a5ebae
ldeveloperl1985 <ldeveloperl1985@gmail.com>
2016-07-28 17:00:55 +0530

    Committing Swagger Code with OTTO-Node

--- app.js
+++ app.js
@@ -1,18 +1,46 @@
 var http = require("http");
-var port = "5053";
+var port = "5053"; // If you want to change the port change in /public/swagger/index.html - "  discoveryUrl: "http://localhost:5053/api-docs.json","for swagger UI
 var express = require('express');
 var router = express.Router();
 var path = require('path');
-
+var swagger = require('swagger-express');
 var routes = require('./routes/index');
 var app = express();
 var server = require('http').Server(app);
 
+app.set('port', process.env.PORT || port);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+//Swagger Settings
+app.use(swagger.init(app, {
+  apiVersion: '1.0',
+  swaggerVersion: '1.0.5',
+  basePath: 'http://localhost:5053',
+  swaggerURL: '/swagger',
+  swaggerJSON: '/api-docs.json',
+  swaggerUI: './public/swagger/',
+  apis: ['./routes/index.js']
+}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.set('development', function(){
+  app.use(express.errorHandler());
+});
+
+//View Engine
 app.set('views', path.join (__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-
+//All Routes
 app.use('/', routes);
+
+
+/*http.createServer(app).listen(port, function(){
+  console.log("Express server listening on port " + port);
+});*/
 
 
 server.listen(port, function () {
--- routes/index.js
+++ routes/index.js
@@ -1,45 +1,93 @@
 var express = require('express');
 var router = express.Router();
 
+
+/**
+ * @swagger
+ * resourcePath: /OTTO
+ * description: Open Trust Taxonomy for Federation Operators
+ */
 router.get('/', function (req, res) {
-      res.writeHead(200, {
-  			'Content-Type': 'text/plain'
-  		});
-  		res.end('Home : 200');
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+  		res.end('Open Trust Taxonomy for Federation Operators : 200');
 });
 
-router.get('/.well-known/openid-configuration', function (req, res) {
-  res.render('index.ejs', { title: "Home", host: req.get('host') });
+/**
+ * @swagger
+ * path: /.well-known/otto-configuration
+ * operations:
+ *   -  httpMethod: GET
+ *      summary: Discovery Endpoint
+ *      notes: Endpoint to return discovery metadata that are hosted by given server.
+ *      nickname: DiscoveryEndpointAPI
+ */
+router.get('/.well-known/otto-configuration', function (req, res) {
+ res.render('index.ejs', { title: "Home", host: req.get('host') });
 });
 
 
+/**
+ * @swagger
+ * path: /federations
+ * operations:
+ *   -  httpMethod: GET
+ *      summary: Federations Endpoint
+ *      notes: Endpoint to return federation metadata or federation IDs that are hosted by given server.
+ *      nickname: FederationsEndpointAPI
+ */
 router.get('/federations', function (req, res) {
       res.writeHead(200, {
   			'Content-Type': 'text/plain'
   		});
-  		res.end('federations : 200');
+  		res.end('Federations Endpoint : 200');
 });
 
+/**
+ * @swagger
+ * path: /federation_entity
+ * operations:
+ *   -  httpMethod: GET
+ *      summary: Federation_Entity Endpoint
+ *      notes:  Endpoint to return federation_entity metadata  that are hosted by given server.
+ *      nickname: Federation_EntityAPI
+ */
 router.get('/federation_entity', function (req, res) {
       res.writeHead(200, {
   			'Content-Type': 'text/plain'
   		});
-  		res.end('federation_entity : 200');
+  		res.end('Federation_Entity Endpoint : 200');
 });
 
+/**
+ * @swagger
+ * path: /org
+ * operations:
+ *   -  httpMethod: GET
+ *      summary: Organization Endpoint
+ *      notes: Endpoint to return organization metadata  that are hosted by given server.
+ *      nickname: OrganizationEndpointAPI
+ */
 router.get('/org', function (req, res) {
       res.writeHead(200, {
   			'Content-Type': 'text/plain'
   		});
-  		res.end('org : 200');
+  		res.end('Organization Endpoint : 200');
 });
 
+/**
+ * @swagger
+ * path: /schema
+ * operations:
+ *   -  httpMethod: GET
+ *      summary: Schema Endpoint
+ *      notes: Endpoint to return schema metadata  that are hosted by given server.
+ *      nickname: SchemaEndpointAPI
+ */
 router.get('/schema', function (req, res) {
       res.writeHead(200, {
   			'Content-Type': 'text/plain'
   		});
-  		res.end('schema : 200');
+  		res.end('Schema Endpoint : 200');
+
 });
-
-
 module.exports = router;
